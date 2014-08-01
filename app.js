9073b1e25b05ad65a2fa270381a612743fb3cd39
anback <anders@swedishtravelmafia.com>
2014-08-01 10:57:41 +0200

    only get the latest

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+
+var moment = require('moment');
+
+console.log(moment().format('YYYY-MM-DD hh:mm:ss'));
 var mubsub = require('mubsub');
 
 var client = mubsub('mongodb://swalo:84kAanan@ds051658.mongolab.com:51658/swalo');
 var channel = client.channel('searches');
-var sqlQuery = "SELECT in_FromCity, in_ToCity, in_DepartureDate, in_ReturnDate, Created FROM tomtom.statisticsairsearchlog where in_ReturnDate != '0001-01-01 00:00:00' order by Created desc limit 3"
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'aragorn.mrorange.local',
@@ -10,9 +13,32 @@ var connection = mysql.createConnection({
   password : 'homermad!'
 });
 
+function GetSearchQuery() {
+
+  var CreateDate = moment().subtract(10, 's').format('YYYY-MM-DD hh:mm:ss');
+  var sqlQuery = "SELECT in_FromCity, in_ToCity, in_DepartureDate, in_ReturnDate, Created FROM tomtom.statisticsairsearchlog where in_ReturnDate != '0001-01-01 00:00:00' and Created > '{0}' order by Created desc limit 10"
+  sqlQuery = sqlQuery.format(CreateDate);
+  return sqlQuery;
+}
+
+// First, checks if it isn't implemented yet.
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
+
 function GetSearchData() {
   console.log("Getting Search Data")
-  connection.query(sqlQuery, function(err, rows, fields) {
+  var sql = GetSearchQuery();
+  console.log(sql);
+  connection.query(sql, function(err, rows, fields) {
     if (err) throw err;
     console.log("Found Data")
     rows.forEach(function(item) {
@@ -30,3 +56,4 @@ channel.on('error', console.error);
 
 GetSearchData();
 setInterval(GetSearchData, 10000 );
+
